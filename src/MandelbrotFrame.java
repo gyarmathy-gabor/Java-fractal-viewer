5b60e2f110f2dfa2f0774a5e0f4342d373e2340d
gyari <gy.gabor03@gmail.com>
2024-09-16 22:06:44 +0200

    Canvas set on Frame

--- src/MandelbrotFrame.java
+++ src/MandelbrotFrame.java
@@ -4,10 +4,13 @@ public class MandelbrotFrame extends JFrame {
     public MandelbrotFrame(){
         this.setSize(800,800);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setLayout(null); //set the layout to manual
 
         //Adding canvas
         Canvas canvas = new Canvas();
         this.add(canvas);
+        canvas.setBounds(0,50,800,750);
+
 
         this.setVisible(true);
     }
