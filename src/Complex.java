0bf513eb085bc421e70fd0931bceea081203275b
gyari <gy.gabor03@gmail.com>
2024-09-16 21:21:24 +0200

    Complex class basics done

---
+++ src/Complex.java
@@ -0,0 +1,30 @@
+public class Complex {
+    private final double real;
+    private final double imaginary;
+
+    public Complex(double r, double i){
+        real = r;
+        imaginary = i;
+    }
+
+    public Complex add(Complex other){
+        return new Complex(this.real+other.real,this.imaginary+other.imaginary);
+    }
+
+    public Complex subtract(Complex other){
+        return new Complex(this.real-other.real,this.imaginary-other.imaginary);
+    }
+
+    public Complex multiply(Complex other){
+        double returnReal = this.real*other.real - this.imaginary*other.imaginary;
+        double returnImag = this.real*other.imaginary + this.imaginary* other.real;
+
+        return new Complex(returnReal,returnImag);
+    }
+
+    public Complex square(){
+        return this.multiply(this);
+    }
+
+
+}
