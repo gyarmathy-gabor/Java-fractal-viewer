f16e2b1f4e743aeb2fdfbe5ed0ab3847750de78d
gyari <gy.gabor03@gmail.com>
2024-09-16 21:35:06 +0200

    MandelbrotFrame constructor basic initialization

--- src/MandelbrotFrame.java
+++ src/MandelbrotFrame.java
@@ -1,5 +1,11 @@
 import javax.swing.*;
 
 public class MandelbrotFrame extends JFrame {
+    public MandelbrotFrame(){
+        this.setSize(800,800);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
+
+        this.setVisible(true);
+    }
 }
