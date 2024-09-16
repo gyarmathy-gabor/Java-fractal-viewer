213f95cf5df844716998b4f2e613ba495ce92597
gyari <gy.gabor03@gmail.com>
2024-09-16 22:32:56 +0200

    MandelbrotCalculator default values

--- src/MandelbrotCalculator.java
+++ src/MandelbrotCalculator.java
@@ -1,3 +1,9 @@
+import java.awt.*;
+
 public class MandelbrotCalculator {
+    static final int MAX_ITERATIONS = 25;
+    static final double ESCAPE_RADIUS = 2;
+    static int round = 0;
+    static Dimension canvasDim =new Dimension(800,750);
 
 }
