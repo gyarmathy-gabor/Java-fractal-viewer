255c03e164a1dd7823c8c8c2bd9979a3f339229b
gyari <gy.gabor03@gmail.com>
2024-09-16 22:02:59 +0200

    Canvas constructor and placeholder color

--- src/Canvas.java
+++ src/Canvas.java
@@ -1,5 +1,10 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Canvas extends JPanel {
 
+    public Canvas(){
+        this.setPreferredSize(new Dimension(700,800));
+        this.setBackground(Color.BLACK);
+    }
 }
