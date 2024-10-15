451b1f16d08c59808481a3624016a320e774e43b
Gyarmathy Gábor <gy.gabor03@gmail.com>
2024-10-15 17:38:42 +0200

    Colours done

--- src/main/java/ColormapPanel.java
+++ src/main/java/ColormapPanel.java
@@ -5,14 +5,30 @@ public class ColormapPanel extends SettingPanel {
     Colormap[] colormaps;
 
     JComboBox<String> maps;
+    ColormapDisplayPanel colorsOfMaps;
+
+
+    JSplitPane Pane;
 
     public ColormapPanel(){
         super();
         colormaps = FileUtils.readColormaps("src/main/resources/colormaps.json");
 
         maps = new JComboBox(colormaps);
-        maps.setBounds(200,200,150,30);
+        maps.setBounds(10,10,150,30);
+
+        colorsOfMaps = new ColormapDisplayPanel();
+        colorsOfMaps.setBounds(10, 40, 400, 50);
+        maps.addActionListener(e -> {
+            int selectedIndex = maps.getSelectedIndex();
+            colorsOfMaps.setColormap(colormaps[selectedIndex]);
+        });
+
         add(maps);
+        add(colorsOfMaps);
+        colorsOfMaps.setColormap(colormaps[maps.getSelectedIndex()]);
+
+
     }
 
     public Colormap getChosenColormap(){
